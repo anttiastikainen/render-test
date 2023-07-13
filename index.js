cf561e10acf6b7fb97211284b9c351e743cc5955
Antti <antti.astikainen@edu.karelia.fi>
2023-07-13 17:56:42 +0300

    switched to using mongodb as database

--- index.js
+++ index.js
@@ -89,21 +89,18 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => { 
     const body = request.body
 
-    if(!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+    if(body.content === undefined) {
+        return response.status(400).json({error: 'content missing'})
     }
     
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 app.use(unknownEndpoint)
 
